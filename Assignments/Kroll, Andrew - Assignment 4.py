b2228ef13b848986b5808b3fc58ef91d07d9dade
Andrew Kroll <hikaridev@users.noreply.github.com>
2019-11-04 19:12:24 -0500

    Moved to Assignments folder.

--- Assignments/Kroll, Andrew - Assignment 4.py
+++ Assignments/Kroll, Andrew - Assignment 4.py
@@ -5,7 +5,7 @@
 # a Human vs. the RPS program (which I’ll call Robbie), compete to see who
 # can get the most points in a match, the most points in the overall
 # tournament, and win the most matches in the tournament.
-import in_out as io
+from Assignments import in_out as io
 import random
 
 
